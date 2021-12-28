1a8d56e82390f91d7a76e488319a43549af86d82
Boris <kolesnikoff.bn@gmail.com>
2021-12-28 11:45:38 +0300

    refactor: изменение работы с mimetypes через стандартную библиотеку mimetypes
    - библиотека реализует весь требуемый функционал, и по этому заменяем вызовы subprocess -> mimetypes

--- sweeper/app/common/utils/mime_typer.py
+++ sweeper/app/common/utils/mime_typer.py
@@ -1,4 +1,4 @@
-import subprocess
+import mimetypes
 from pathlib import Path
 
 from sweeper.app.common.types import MIME
@@ -6,11 +6,8 @@ from sweeper.app.common.types import MIME
 
 class MimeTyper:
     def from_file(self, source_file_path: Path) -> MIME:
-        stdout_result = subprocess.run(
-            ["file", "-b", "--mime-type", source_file_path.as_posix()], capture_output=True
-        ).stdout.decode()
-
-        return self._get_major_mimetype(stdout_result)
+        mime_type, _ = mimetypes.guess_type(source_file_path.as_posix())
+        return self._get_major_mimetype(mime_type)
 
     def _get_major_mimetype(self, stdout: str) -> MIME:
         """Разделение полученного результат от команды `file` на основной тип и подтип. Возрвщаем основной тип.
